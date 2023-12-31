cfa2dcba00098b714f4d88216a4cbd6f357f0060
PhongGodFather <dinhquangphong365@gmail.com>
2023-12-31 11:17:40 +0700

    update sample templates and statics

--- db.py
+++ db.py
@@ -1,8 +1,15 @@
 import pyodbc
+from hashlib import sha256
+key = open("ServernameHere.txt",'r')
+contents = key.read()
+print(contents)
+
 conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
-                      'Server=LAPTOP-6KGH4NMD;'
+                      f'Server={contents};'
                       'Database=QLPHONGKHAMNHAKHOA;'
                       'Trusted_Connection=yes;', autocommit=True) # autocommit for fast transaction
 
-
 cursor = conn.cursor()
+
+def hashPass(password: str) -> str:
+    return sha256(password.encode()).hexdigest()
